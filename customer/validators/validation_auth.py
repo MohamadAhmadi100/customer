fc0f7d356e72e7011292c1dbc91341b43860adc8
root <moafi@aasood.com>
2022-01-23 16:51:13 +0330

    inheritence mobile number on other class

--- customer/validators/validation_auth.py
+++ customer/validators/validation_auth.py
@@ -3,8 +3,13 @@ import re
 from fastapi import HTTPException, Query
 from pydantic import BaseModel, validator, Field
 
+"""
+handling generate form(mobile number) validations on frontend side &&& validation data on backend Side
+"""
+
 
 class CustomerAuth(BaseModel):
+    # generate frontend validation form
     customer_phone_number: str = Field(
         title="شماره موبایل",
         alias="customerPhoneNumber",
@@ -19,6 +24,9 @@ class CustomerAuth(BaseModel):
         regexPattern="^09[0-9]{9}$",
     )
 
+    # validation phone number on backend side
+    # TODO validate code type
+
     @validator("customer_phone_number")
     def validate_phone_num(cls, phone_number):
         pattern = r"^09[0-9]{9}$"
@@ -28,21 +36,7 @@ class CustomerAuth(BaseModel):
         return phone_number
 
 
-class CustomerVerifyOTP(BaseModel):
-    customer_phone_number: str = Field(
-        title="شماره موبایل",
-        alias="customerPhoneNumber",
-        name="customerPhoneNumber",
-        placeholder="09371654426",
-        description="phone number must be string and len 11 character",
-        minLength=11,
-        maxLength=11,
-        dataType="str",
-        type="tel",
-        isRquired=True,
-        regexPattern="^09[0-9]{9}$",
-    )
-
+class CustomerVerifyOTP(CustomerAuth):
     customer_code: str = Field(
         title="رمز یک بار مصرف",
         description="otp code must be string and len 4 character",
@@ -57,14 +51,6 @@ class CustomerVerifyOTP(BaseModel):
         regexPattern="^[0-9]{4}$",
     )
 
-    @validator("customer_phone_number")
-    def validate_phone_num(cls, phone_number):
-        pattern = r"^09[0-9]{9}$"
-        match = re.fullmatch(pattern, phone_number)
-        if not match:
-            raise HTTPException(status_code=422, detail={"error": "Please enter a valid phone number"})
-        return phone_number
-
     @validator("customer_code")
     def validate_otp(cls, code):
         pattern = r"^[0-9]{4}$"
@@ -74,20 +60,7 @@ class CustomerVerifyOTP(BaseModel):
         return code
 
 
-class CustomerVerifyPassword(BaseModel):
-    customer_phone_number: str = Field(
-        title="شماره موبایل",
-        alias="customerPhoneNumber",
-        name="customerPhoneNumber",
-        placeholder="09371654426",
-        description="phone number must be string and len 11 character",
-        minLength=11,
-        maxLength=11,
-        dataType="string",
-        type="tel",
-        isRquired=True,
-        regexPattern="^09[0-9]{9}$",
-    )
+class CustomerVerifyPassword(CustomerAuth):
     customer_password: str = Field(
         title="رمز عبور",
         alias="customerPassword",
@@ -101,23 +74,6 @@ class CustomerVerifyPassword(BaseModel):
         isRquired=True,
         regexPattern="^^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,32}$",
     )
-    # customer_login_type: str = Field(
-    #     title="شیوه ورود کاربر",
-    #     alias="customerLoginType",
-    #     name="customerLoginType",
-    #     description="customerLoginType must be `otp` or `password`",
-    #     dataType="string",
-    #     type="hidden",
-    #     isRquired=True,
-    # )
-
-    @validator("customer_phone_number")
-    def validate_phone_num(cls, phone_number):
-        pattern = r"^09[0-9]{9}$"
-        match = re.fullmatch(pattern, phone_number)
-        if not match:
-            raise HTTPException(status_code=422, detail={"error": "Please enter a valid phone number"})
-        return phone_number
 
     @validator("customer_password")
     def validate_password(cls, code):
@@ -126,9 +82,3 @@ class CustomerVerifyPassword(BaseModel):
         if not match:
             raise HTTPException(status_code=422, detail={"error": "Please enter a valid password"})
         return code
-    #
-    # @validator("customer_login_type")
-    # def validate_password(cls, code_type):
-    #     if code_type == "otp" and code_type == "password":
-    #         raise HTTPException(status_code=422, detail={"error": "Please enter a valid login type"})
-    #     return code_type

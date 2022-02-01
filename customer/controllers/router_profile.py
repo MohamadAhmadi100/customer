6ebf86aa07026a0b54373a3184cf7c739027b2a1
root <moafi@aasood.com>
2022-02-01 12:19:25 +0330

    fix conflict

--- customer/controllers/router_profile.py
+++ customer/controllers/router_profile.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends
 from fastapi import Response, status
 
 from customer.models.model_register import Customer
+from customer.models.model_register import Customer
 from customer.mudoles.auth import AuthHandler
 from customer.validators import validation_profile, validation_auth
 
@@ -12,30 +13,39 @@ router_profile = APIRouter(
 
 auth_handler = AuthHandler()
 
+@router_profile.get("/")
+def get_profile(
+        response: Response,
+        auth_header=Depends(auth_handler.check_current_user_tokens),
 
-# @router_profile.post("/deactivate_user")
-# def register_validation_generator(
-#         response: Response,
-#         value: validation_auth.CustomerAuth,
-# ):
-#     customer = Customer(value.customer_phone_number)
-#     customer.set_activity()
-#     return response.status_code
-#
-#
-# @router_profile.post("/set-password/")
-# def set_password(
-#         value: validation_profile.CustomerSetPassword,
-#         response: Response,
-#         auth_header=Depends(auth_handler.check_current_user_tokens)
-# ):
-#     customer = Customer(phone_number=value.customer_phone_number)
-#     if customer.save():
-#         response.status_code = status.HTTP_200_OK
-#         response.headers["accessToken"] = auth_header["access_token"]
-#         response.headers["refreshToken"] = auth_header["refresh_token"]
-#         message = {"message": "رمز عبور با موفقیت تغییر کرد"}
-#     else:
-#         response.status_code = status.HTTP_417_EXPECTATION_FAILED
-#         message = {"massage": "تغییر رمز عبور با مشکل مواجه شد لطفا دوباره سعی کنید"}
-#     return message
+
+):
+    user,header = auth_header
+    return user
+
+
+@router_profile.put("/edit-data")
+def edit_profile_data(
+        response: Response,
+        auth_header=Depends(auth_handler.check_current_user_tokens),
+):
+
+    pass
+
+@router_profile.post("/set-password/")
+def set_password(
+        value: validation_profile.CustomerSetPassword,
+        response: Response,
+        auth_header=Depends(auth_handler.check_current_user_tokens)
+):
+    customer = Customer(phone_number=value.customer_phone_number)
+    if customer.save():
+        response.status_code = status.HTTP_200_OK
+        response.headers["accessToken"] = auth_header["access_token"]
+        response.headers["refreshToken"] = auth_header["refresh_token"]
+        message = {"message": "رمز عبور با موفقیت تغییر کرد"}
+    else:
+        response.status_code = status.HTTP_417_EXPECTATION_FAILED
+        message = {"massage": "تغییر رمز عبور با مشکل مواجه شد لطفا دوباره سعی کنید"}
+    return message
+>>>>>>> Stashed changes

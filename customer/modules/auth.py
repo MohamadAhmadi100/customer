b39a34512482cae9db6abdfde824f1ea40d1505f
ehsan <ehsankhorram72@gmail.com>
2022-02-20 17:29:58 +0330

    cleaned auth module

--- customer/modules/auth.py
+++ customer/modules/auth.py
@@ -5,9 +5,6 @@ from passlib.context import CryptContext
 
 class AuthHandler:
     pwd_context = CryptContext(schemes=['bcrypt'], deprecated="auto")
-    SECRET_KEY = "a5cad81912ad25eb12920bf6357d799773887f77291fec95c345fd136078bf2c"
-    refresh_exp = timedelta(days=1)
-    access_exp = timedelta(days=0, minutes=20)
 
     def generate_hash_password(self, password: str) -> str:
         return self.pwd_context.hash(password)

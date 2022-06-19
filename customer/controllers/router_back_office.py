b1e60b8970a3aacff68bc297311d1ef637b135db
Mohamad <mohammadahmadidezhnet@gmail.com>
2022-06-19 13:17:52 +0430

    add crm informal

--- customer/controllers/router_back_office.py
+++ customer/controllers/router_back_office.py
@@ -2210,7 +2210,6 @@ def crm_get_profile(customer_phone_number: dict):
     customer_phone_number = customer_phone_number.get('phone_number')
     profile = Profile({"customer_phone_number": customer_phone_number})
     if result := profile.get_profile_data():
-        print(result)
         customer = {grid_attribute: result.get(grid_attribute) or None for grid_attribute in VALID_PROFILE_KEYS}
         return {"success": True, "message": customer, "status_code": 200}
     return {"success": False, "error": "اطلاعاتی برای کاربر وجود ندارد", "status_code": 401}

9bfaf3e44d93243a0a7a365813cf96528aa165ca
zhra-jm <zahrajam1999@gmail.com>
2022-07-19 13:00:11 +0430

    listener edited

--- customer/controllers/main_controller.py
+++
@@ -1,4 +0,0 @@
-from customer.controllers.router_auth import *
-from customer.controllers.router_profile import *
-from customer.controllers.router_register import *
-from customer.controllers.router_back_office import *
